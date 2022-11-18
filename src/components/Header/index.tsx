bd8f8f44eb9fbff54af83dad4b942f137cea64b3
Renan <renan23eduardo@gmail.com>
2022-11-18 08:24:22 -0300

    Create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import './styles';
+import { Container, Content } from './styles';
+
+export function Header(){
+  return(
+    <Container>
+      <Content>
+        <div className="page-details">
+          <h1>Pedidos</h1>
+          <h2> Acompanhe os pedidos dos clientes</h2>
+        </div>
+
+        <img src={logo} alt="WAITERAPP" />
+      </Content>
+    </Container>
+  );
+}
